ace4a4f9df4066a9b9149f35518121ebbf3e109c
M. Nuraminudin (Laptop) <adin.5829@gmail.com>
2024-06-13 00:45:32 +0700

    add model prov,kota,kec

---
+++ app/Models/ProvinsiModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProvinsiModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'provinces';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
